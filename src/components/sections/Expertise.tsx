aadfabf5254a98bf63b11adf57a61e9be8b191fe
Gianni James Favaretto <cpe@justscore.com>
2026-01-27 13:16:06 +0000

    responsive 27/01/26

--- src/components/sections/Expertise.tsx
+++ src/components/sections/Expertise.tsx
@@ -13,15 +13,15 @@ export function Expertise({ items }: ExpertiseContent) {
     ));
 
   return (
-    <Section tone="dark" className="-mt-40">
+    <Section tone="dark" className="-mt-16 md:-mt-40">
       <Container>
       <Grid>
-        <div className="col-start-4 col-span-7 flex flex-col gap-8 pb-8">
-          <div className="flex flex-col gap-4 md:flex-row">
+        <div className="col-span-4 flex flex-col gap-6 pb-6 md:col-start-4 md:col-span-7 md:gap-8 md:pb-8">
+          <div className="flex flex-col gap-6 md:flex-row md:gap-4">
             {items.map((item) => (
-              <div key={item.title} className="flex-1 p-4 pl-0">
+              <div key={item.title} className="md:flex-1 p-4 md:p-0">
                 <div className="flex items-center gap-4">
-                  <span className="flex size-16 items-center justify-center rounded-full">
+                  <span className="flex size-12 items-center justify-center rounded-full bg-white/5 md:size-16 md:bg-transparent">
                     <Icon src={item.icon} alt={item.title} size={20} />
                   </span>
                   <h3 className="copy-lg font-medium text-neutral-lighter">
