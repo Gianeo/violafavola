ac8ec823d0df8efd4776bafe594402dc78094097
Gianni James Favaretto <cpe@justscore.com>
2026-02-11 12:56:54 +0000

    MS Clarity installed 11/02/26

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 
 const generalSans = localFont({
@@ -40,6 +41,13 @@ export default function RootLayout({
         className={`${generalSans.variable} ${generalSans.className} antialiased`}
         suppressHydrationWarning
       >
+        <Script id="clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "vfppfaj1qm");`}
+        </Script>
         {children}
       </body>
     </html>
